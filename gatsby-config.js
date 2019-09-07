8ab4548bbab06344453ed8015d14758008b6e5e3
Mohammad Hasani <thebrodmann@gmail.com>
2019-09-08 00:22:30 +0430

    feat(gatsby/config): update site metadata

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,9 +6,9 @@ function root(...args) {
 
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby Default Starter',
-    description: 'Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.',
-    author: '@gatsbyjs',
+    title: 'SoftwareTalks - Next Version',
+    description: 'The next version of SoftwareTalks website.',
+    author: 'Mohammad Hasani <thebrodmann@gmail.com> (https://github.com/thebrodmann)',
   },
   plugins: [
     'gatsby-plugin-react-helmet',

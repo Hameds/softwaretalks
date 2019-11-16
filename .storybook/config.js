d5cb3fc187ef97bc44f5f73bfcc340e5183f40d8
Mohammad Hasani <thebrodmann@gmail.com>
2019-11-16 20:29:31 +0330

    test: add container decorator to stories

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,10 +1,13 @@
+import React from 'react'
 import { configure, addDecorator } from '@storybook/react'
 import { withInfo } from '@storybook/addon-info'
 
 import 'normalize.css'
 import '../src/scss/main.scss'
+import '../src/scss/storybook.scss'
 
 addDecorator(withInfo)
+addDecorator(storyFn => <div className="container">{storyFn()}</div>)
 
 // automatically import all files ending in *.stories.tsx
 configure(require.context('../stories', true, /\.stories\.tsx$/), module)

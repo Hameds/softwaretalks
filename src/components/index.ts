7b5d441db0b0ef74632742a92d2a771e4364fe9d
Mohammad Hasani <thebrodmann@gmail.com>
2019-11-16 21:42:46 +0330

    feat: add paragraph component
    closes #41

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,5 +1,6 @@
 import * as Preview from './preview'
 import * as Button from './button'
 import * as Heading from './heading'
+import * as Paragraph from './paragraph'
 
-export { Preview, Button, Heading }
+export { Preview, Button, Heading, Paragraph }
---
+++ src/components/paragraph.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import cc from 'classcat'
+
+import { defineDisplayName } from '../utils'
+
+type Props = JSX.IntrinsicElements['p']
+
+const classNames = {
+  block: 'c-paragraph',
+}
+
+export function component({ className: customClassName, ...props }: Props) {
+  const className = cc([classNames.block, customClassName])
+
+  return <p {...props} className={className} />
+}
+
+defineDisplayName('Paragraph', { component })

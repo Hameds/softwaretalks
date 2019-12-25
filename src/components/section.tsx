c2d486fa06bcb6615e4e8bda487bc0f51d63a41c
Mohammad Hasani <thebrodmann@gmail.com>
2019-12-25 23:26:02 +0330

    feat: add section component

---
+++ src/components/section.tsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import cc from 'classcat'
+
+import * as Heading from './heading'
+
+const classNames = {
+  elements: {
+    container: 'c-section__container',
+    headline: 'c-section__headline',
+  },
+}
+
+type Props = React.PropsWithChildren<{
+  headline?: string
+}>
+
+export function component({ headline, children }: Props) {
+  const containerClassName = cc(['l-container', classNames.elements.container])
+
+  return (
+    <section className="c-section">
+      <div className={containerClassName}>
+        {headline && (
+          <Heading.H2 className={classNames.elements.headline}>
+            {headline}
+          </Heading.H2>
+        )}
+        {children}
+      </div>
+    </section>
+  )
+}
